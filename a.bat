a42d2884e518075862dfdbd0dee5beb1f12efd12
dillip-meher <dillip_meher@hotmail.com>
2023-12-01 20:15:03 +0530

    testa

--- a.bat
+++ a.bat
@@ -1,4 +1,7 @@
+@echo off #################### Start of Task ####################
+
 git add -A
 git commit -m "testa"
 git push
-@echo off #################### End of Task ####################
+
+@echo off ##################### End of Task ######################
