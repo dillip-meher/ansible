8f6b01533d0b57229df3fb5a1ad5cf404ea69e99
dillip-meher <dillip_meher@hotmail.com>
2023-12-05 18:44:41 +0530

    Tomcat Installtion

---
+++ tomcat/a.bat
@@ -0,0 +1,7 @@
+@echo ################################## Tomcat Installtion #################################################
+
+git add -A
+git commit -m "Tomcat Installtion"
+git push
+
+@echo ################################## Tomcat Installtion #################################################
